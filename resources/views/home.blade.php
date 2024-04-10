8074ef859e8d98f72e8f3045cb97931c97e4b147
simoneditaranto <simodita99@gmail.com>
2024-04-10 16:35:14 +0200

    feat: stampa in pagina di tutti i dati dei treni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts/app')
 
 @section('content')
+
     <div class="container py-5">
         <h1>Treni</h1>
+
+        <div class="trains d-flex flex-wrap gap-3">
+
+            @foreach($trains as $train)
+            <div class="card" style="width: 15rem;">
+                {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                <div class="card-body">
+                    <h5 class="card-title">{{$train->azienda}}</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{{$train->s_partenza}} - {{$train->s_arrivo}}</li>
+                    <li class="list-group-item">{{$train->orario_partenza}} - {{$train->orario_arrivo}}</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">{{$train->codice_treno}}</li>
+                    <li class="list-group-item">{{$train->n_carrozze}}</li>
+                    <li class="list-group-item">{{$train->cancellato? 'cancellato' : 'attivo'}}</li>
+                    @if(!$train->cancellato)
+                        <li class="list-group-item">{{$train->in_orario? 'in orario' : 'in ritardo'}}</li>
+                    @endif
+                </ul>
+                <div class="card-body">
+                    <a href="#" class="card-link">Acquista</a>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
     </div>
+
 @endsection
